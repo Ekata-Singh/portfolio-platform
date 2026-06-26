b055e2d3310f538eed9fa354d908b8bd22a50f04
Ekata <ekatasingh290304@gmail.com>
2026-06-27 01:10:16 +0530

    refactor: clean generated project structure

---
+++ backend/src/main/java/com/ektasingh/portfolio/PortfolioPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.ektasingh.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioPlatformApplication{
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ektasingh/portfolio/PortfolioPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ektasingh.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
